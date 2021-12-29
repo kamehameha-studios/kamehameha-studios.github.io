d817c06c2ba65dc37f7c3e5126794c894f7ad6d3
Bosco <95505913+boscokwong@users.noreply.github.com>
2021-12-29 09:42:57 -0600

    Create index.html.js

---
+++ index.html.js
@@ -0,0 +1,6 @@
+function WriteCookie() {
+        cookievalue = "kamehameha-studios";
+        
+        document.cookie = "domain=" + "goku.github.io";
+        document.cookie = "path=" + "/;";
+        document.cookie = cookievalue + "=" + "none";
