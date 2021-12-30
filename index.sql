05fe82864f6118f51210dbc33e54e86fe543ebbd
Bosco <95505913+boscokwong@users.noreply.github.com>
2021-12-30 09:37:28 -0600

    Create index.sql

---
+++ index.sql
@@ -0,0 +1,3 @@
+CREATE TABLE new_users (id INTEGER PRIMARY KEY, username TEXT, password REAL);
+
+CREATE TABLE other_users (id INTEGER PRIMARY KEY, username TEXT, password REAL, email REAL, real_name TEXT);
