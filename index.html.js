e39e49f6d4f0f1469c89f7bf4b82aff8bd66d1ce
Bosco <95505913+boscokwong@users.noreply.github.com>
2022-01-02 08:15:57 -0600

    Delete index.html.js

--- index.html.js
+++
@@ -1,6 +0,0 @@
-function WriteCookie() {
-        cookievalue = "kamehameha-studios";
-        
-        document.cookie = "domain=" + "goku.github.io";
-        document.cookie = "path=" + "/;";
-        document.cookie = cookievalue + "=" + "none";
