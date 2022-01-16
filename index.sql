e962188da0b2ede16491794740553baf5ddb9f1e
Bosco Kwong <95505913+boscokwong@users.noreply.github.com>
2022-01-16 15:51:30 -0600

    Update index.sql

--- index.sql
+++ index.sql
@@ -1,3 +1,18 @@
-CREATE TABLE new_users (id INTEGER PRIMARY KEY, username TEXT, password REAL);
+CREATE TABLE new_users (
+  id INTEGER PRIMARY KEY, 
+  username TEXT, 
+  password REAL
+);
 
-CREATE TABLE other_users (id INTEGER PRIMARY KEY, username TEXT, password REAL, email REAL, real_name TEXT);
+CREATE TABLE other_users (
+  id INTEGER PRIMARY KEY, 
+  username TEXT, 
+  password REAL, 
+  email REAL, 
+  real_name TEXT
+);
+INSERT INTO new_users VALUES (
+  1, 
+  "Bosco", 
+  "opP4^yu86HhjK"
+);
