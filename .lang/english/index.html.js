4d014c5dbcdca316cc28d3ab0d7bce16bb5aac92
Bosco Kwong <95505913+boscokwong@users.noreply.github.com>
2022-01-18 16:08:07 -0600

    Update and rename index.html.js to .lang/english/index.html.js

---
+++ .lang/english/index.html.js
@@ -0,0 +1,9 @@
+function WriteCookie() {
+  cookievalue = "kamehameha-studios";
+
+  document.cookie = "domain=" + "goku.github.io";
+  document.cookie = "path=" + "/;";
+  document.cookie = cookievalue + "=" + "none";
+var alert = function() {
+  alert("You are logged in as a Guest. Plese Log in or Sign up to view you things.");
+}
--- index.html.js
+++
@@ -1,9 +0,0 @@
-      function WriteCookie() {
-        cookievalue = "kamehameha-studios";
-        
-        document.cookie = "domain=" + "goku.github.io";
-        document.cookie = "path=" + "/;";
-        document.cookie = cookievalue + "=" + "none";
-        var alert = function() {
-          alert("You are logged in as a Guest. Plese Log in or Sign up to view you things.");
-        }
