afcdb231fe296b0d90a14f48093a91100ab01bf5
Bosco Kwong <95505913+boscokwong@users.noreply.github.com>
2022-01-22 08:09:02 -0600

    fix all errors!!!

--- .home/.other/index.sql
+++ .home/.other/index.sql
@@ -1,17 +1,10 @@
-CREATE TABLE new_users (
+CREATE TABLE users (
   id INTEGER PRIMARY KEY, 
   username TEXT, 
   password REAL
 );
 
-CREATE TABLE other_users (
-  id INTEGER PRIMARY KEY, 
-  username TEXT, 
-  password REAL, 
-  email REAL, 
-  real_name TEXT
-);
-INSERT INTO new_users VALUES (
+INSERT INTO users VALUES (
   1, 
   "Bosco", 
   "opP4^yu86HhjK"
--- index.html.js
+++ index.html.js
@@ -4,6 +4,7 @@ function WriteCookie() {
   document.cookie = "domain=" + "goku.github.io";
   document.cookie = "path=" + "/;";
   document.cookie = cookievalue + "=" + "none";
+}
 var alert = function() {
   alert("You are logged in as a Guest. Plese Log in or Sign up to view you things.");
 }
