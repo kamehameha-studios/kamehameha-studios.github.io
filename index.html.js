264e7d61229f299ba4ce552eb566acc99add50a0
Bosco Kwong <95505913+boscokwong@users.noreply.github.com>
2022-01-24 17:35:52 -0600

    .

--- index.html.js
+++ index.html.js
@@ -1,10 +1,9 @@
 function WriteCookie() {
   cookievalue = "kamehameha-studios";
-
-  document.cookie = "domain=" + "goku.github.io";
-  document.cookie = "path=" + "/;";
-  document.cookie = cookievalue + "=" + "none";
+  
+  document.cookie = name = "wiki"; content = "none"; domain = "kamehameha-studios.github.io"; path = "/"; expires = "When browsing session ends";;
+  document.cookie = name = "guest"; content = "guest of " + cookievalue; domain = "kamehameha-studios.github.io"; path = "/"; expires = "When browsing session ends";;
 }
 function alert() {
-  alert("You are logged in as a Guest. Plese Log in or Sign up to view you things.");
+  alert("You are logged in as a Guest." + "Plese Log in or Sign up to view you things.");
 }
