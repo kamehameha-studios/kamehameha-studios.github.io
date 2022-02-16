d0683ddb8c06d86753879005d409c190d5e62d98
Bosco Kwong <95505913+boscokwong@users.noreply.github.com>
2022-02-16 16:32:44 -0600

    add `;`

--- index.html.js
+++ index.html.js
@@ -3,10 +3,10 @@ function WriteCookie() {
   
   document.cookie = name = "wiki"; content = "none"; domain = "kamehameha-studios.github.io"; path = "/"; expires = "When browsing session ends";;
   document.cookie = name = "guest"; content = "guest of " + cookievalue; domain = "kamehameha-studios.github.io"; path = "/"; expires = "When browsing session ends";;
-}
+};
 function alert() {
   alert("You are logged in as a Guest." + "Please Log in or Sign up to view you things.");
-}
+};
 var programCode = function(processingInstance) {
   with (processingInstance) {
     size(400, 400); 
